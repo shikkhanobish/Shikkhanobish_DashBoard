6f365fdd135cf8c49397152ecd62d790249d90e7
Mahir Muzahid <shikkhanobish.github@gmail.com>
2021-12-10 23:50:34 +0600

    Assign Task

---
+++ ShikkhanobishBlazor/Models/dataentryOperatorTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishBlazor.Models
+{
+    public class dataentryOperatorTask
+    {
+        public int userID { get; set; }
+        public string userName { get; set; }
+        public string taskID { get; set; }
+        public int chapterID { get; set; }
+        public string MCQNumbers { get; set; }
+        public string date { get; set; }
+        public string chapterName { get; set; }
+        public string isTaskSub { get; set; }
+        public string isTaskSubColor { get; set; }
+        public string isTaskApp { get; set; }
+        public string isTaskAppColor { get; set; }
+        public string Response { get; set; }
+    }
+}
