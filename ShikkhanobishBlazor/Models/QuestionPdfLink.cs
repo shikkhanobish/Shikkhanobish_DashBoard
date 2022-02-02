c8c894c7f2107b7cf21c764c829f4af2371ffde5
shikkhanobish <shikkhanobish.github@gmail.com>
2022-02-02 13:06:39 +0600

    Assign Task added

--- ShikkhanobishBlazor/Models/QuestionPdfLink.cs
+++ ShikkhanobishBlazor/Models/QuestionPdfLink.cs
@@ -7,6 +7,9 @@ namespace ShikkhanobishBlazor.Models
 {
     public class QuestionPdfLink
     {
+        public string className { get; set; }
+        public string subjectName { get; set; }
+        public string chapterName { get; set; }
         public int chapterID { get; set; }
         public string link { get; set; }
         public int noOfQues { get; set; }
